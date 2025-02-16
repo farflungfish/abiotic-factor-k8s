ccb0bf5059cd6e628c6951dd50c3040e6f31b1d4
Doug Rae <rae.doug@gmail.com>
2025-02-16 15:56:27 +0000

    init

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+SetUsePerfThreads="-useperfthreads "
+if [[ $UsePerfThreads == "false" ]]; then
+    SetUsePerfThreads=""
+fi
+
+SetNoAsyncLoadingThread="-NoAsyncLoadingThread "
+if [[ $NoAsyncLoadingThread == "false" ]]; then
+    SetNoAsyncLoadingThread=""
+fi
+
+MaxServerPlayers="${MaxServerPlayers:-6}"
+Port="${Port:-7777}"
+QueryPort="${QueryPort:-27015}"
+ServerPassword="${ServerPassword:-password}"
+SteamServerName="${SteamServerName:-LinuxServer}"
+WorldSaveName="${WorldSaveName:-Cascade}"
+AdditionalArgs="${AdditionalArgs:-}"
+
+pushd /server/AbioticFactor/Binaries/Win64 > /dev/null
+wine AbioticFactorServer-Win64-Shipping.exe $SetUsePerfThreads$SetNoAsyncLoadingThread-MaxServerPlayers=$MaxServerPlayers \
+    -PORT=$Port -QueryPort=$QueryPort -ServerPassword=$ServerPassword \
+    -SteamServerName="$SteamServerName" -WorldSaveName="$WorldSaveName" -tcp $AdditionalArgs
+popd > /dev/null

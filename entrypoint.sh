1451d572168801eaad66d5ba35846c67c001d038
Doug Rae <rae.doug@gmail.com>
2025-02-16 16:06:25 +0000

    added variable for running directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,9 @@ ServerPassword="${ServerPassword:-password}"
 SteamServerName="${SteamServerName:-LinuxServer}"
 WorldSaveName="${WorldSaveName:-Cascade}"
 AdditionalArgs="${AdditionalArgs:-}"
+Directory="${Directory:-server}"
 
-pushd /server/AbioticFactor/Binaries/Win64 > /dev/null
+pushd /$Directory/AbioticFactor/Binaries/Win64 > /dev/null
 wine AbioticFactorServer-Win64-Shipping.exe $SetUsePerfThreads$SetNoAsyncLoadingThread-MaxServerPlayers=$MaxServerPlayers \
     -PORT=$Port -QueryPort=$QueryPort -ServerPassword=$ServerPassword \
     -SteamServerName="$SteamServerName" -WorldSaveName="$WorldSaveName" -tcp $AdditionalArgs
